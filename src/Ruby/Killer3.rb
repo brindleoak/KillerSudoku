6d358f1d091041b81dbc6ea9075deff4944c6ab3
sandymarshall <77060331+sandymarshall@users.noreply.github.com>
2021-01-23 20:55:59 +0000

    Add files via upload
    Optimised Version

---
+++ src/Ruby/Killer3.rb
@@ -0,0 +1,66 @@
+require 'csv'
+
+def sameRow(i,j)
+	return (i/9)>>0 == (j/9)>>0
+end
+
+def sameCol(k,l)
+	return (k-l)%9 == 0
+end
+
+def sameBlock(m,n)
+	return ((m/27)>>0 == (n/27)>>0 && ((m%9)/3)>>0 == ((n%9)/3)>>0)
+end
+
+def checkRow(aRow, position, value) 
+	pos = aRow.index(0)
+	if pos !=nil
+		selectedParm = $parms[position]
+		if selectedParm != nil
+			total = selectedParm.slice(1..-2).collect{|e| aRow[e]}.reduce(value){ |sum, num| sum + num }
+			if total != selectedParm[0] 
+				return false
+			end
+		end
+	end			
+	return true
+end
+
+def recursiveCheck(inBoard)
+	pos = inBoard.index(0)
+	if pos == nil
+		puts "#{inBoard}"
+		puts "#{Time.now}"
+		exit!(true)
+	else	
+		excludedNumbers = Hash.new
+		x = 0
+		y = 1
+
+		while x < pos 
+			if sameRow(pos, x) || sameCol(pos, x) || sameBlock(pos, x) 
+				excludedNumbers[inBoard[x]] = true
+			end
+			x += 1
+		end
+
+		while y < 10 
+			if excludedNumbers[y] == nil 
+				
+				if checkRow(inBoard,pos,y) 
+				    newBoard = inBoard.dup() 
+					newBoard[pos] = y 
+					recursiveCheck(newBoard)
+				end	
+			end
+		y += 1
+		end
+	end
+	return true
+end
+
+puts "#{Time.now}"
+$parms = CSV.read("/home/sandy/puzzle.csv",converters: :integer).to_h{ |x| [x.last(), x]}
+nums = Array.new(81) {|e| e = 0}
+puts "#{$parms}"
+recursiveCheck(nums)
